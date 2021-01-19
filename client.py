58a370f7342e1e0d58c84f65784fcb1a8a4cc198
HaoHuynh <hao152903@gmail.com>
2021-01-19 20:39:12 +0700

    19/1/2021

--- client.py
+++ client.py
@@ -14,35 +14,35 @@ from matplotlib import pyplot
 from PIL import Image
 
 # construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-s", "--server-ip", required=True,
-	help="ip address of the server to which the client will connect")
-args = vars(ap.parse_args())
-# initialize the ImageSender object with the socket address of the
-# server
-sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
-	args["server_ip"]))
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-s", "--server-ip", required=True,
+# 	help="ip address of the server to which the client will connect")
+# args = vars(ap.parse_args())
+# # initialize the ImageSender object with the socket address of the
+# # server
+# sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
+# 	args["server_ip"]))
 
-rpiName = socket.gethostname ()
-vs = VideoStream (src = 0).start ()
-# vs = VideoStream(src=0).start()
-time.sleep (2.0)
+# rpiName = socket.gethostname ()
+# vs = VideoStream (src = 0).start ()
+# # vs = VideoStream(src=0).start()
+# time.sleep (2.0)
 
-while True:
-    # read the frame from the camera and send it to the server
-    frame = vs.read ()
-    sender.send_image (rpiName, frame)
+# while True:
+#     # read the frame from the camera and send it to the server
+#     frame = vs.read ()
+#     sender.send_image (rpiName, frame)
 
-# #Convert image into nparray
-# image = Image.open('/Users/macos/Downloads/125963723_710602699579378_2583582351400280391_o.jpg')
-# # convert image to numpy array
-# data = asarray(image)
-#
-# sender = imagezmq.ImageSender()
-# image_window_name = 'From Sender'
-# while True:  # press Ctrl-C to stop image sending program
-#     # Increment a counter and print it's value to console
-#     print('Sending')
-#     # Add counter value to the image and send it to the queue
-#     sender.send_image(image_window_name, data)
-#     time.sleep(1)
+#Convert image into nparray
+image = Image.open('/Users/macos/Downloads/125963723_710602699579378_2583582351400280391_o.jpg')
+# convert image to numpy array
+data = asarray(image)
+
+sender = imagezmq.ImageSender()
+image_window_name = 'From Sender'
+while True:  # press Ctrl-C to stop image sending program
+    # Increment a counter and print it's value to console
+    print('Sending')
+    # Add counter value to the image and send it to the queue
+    sender.send_image(image_window_name, data)
+    time.sleep(1)
