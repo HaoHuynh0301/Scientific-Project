f6a215614d1e1bc4f607ad658874812c98c99528
HaoHuynh <hao152903@gmail.com>
2021-01-25 17:00:54 +0700

    25/1/2021

--- test_client_save_video.py
+++ test_client_save_video.py
@@ -5,17 +5,17 @@ import cv2
 import time
 from imutils.video import VideoStream
 
-server_ip = "192.168.1.11"
+server_ip = "192.168.1.8"
 sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(server_ip))
 rpiName = socket.gethostname()
-vs = VideoStream(src=0).start()
-time.sleep(1.0)
+# vs = VideoStream(src=0).start()
+# time.sleep(1.0)
 
 #define size for saving video
 # Get current width of frame
 frame_width = 680
 frame_height = 480
-fps = 0.0
+fps = 15.0
 
 video_capture = cv2.VideoCapture(0)
 video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
@@ -29,17 +29,17 @@ result = cv2.VideoWriter('test.mp4', fourcc, fps, size)
 while True:
     ret, frame = video_capture.read()
     if ret==True:
-        print('Write Frame')
-        cv2.imshow("Frame", frame)
+        # print('Write Frame')
+        # cv2.imshow("Frame", frame)
         result.write(frame)
-        # sender.send_image(rpiName, frame)
+        sender.send_image(rpiName, frame)
         key = cv2.waitKey (1) & 0xFF
         if key == ord("q"):
             break
     else:
         break
 
-video_capture.release()
+# video_capture.release()
 result.release()
 # do a bit of cleanup
 cv2.destroyAllWindows()
