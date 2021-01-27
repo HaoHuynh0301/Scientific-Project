de4d51f3f57d713d91a0d349b0df39e4392f19d2
HaoHuynh <hao152903@gmail.com>
2021-01-27 14:48:02 +0700

    27/1/2021

--- client.py
+++ client.py
@@ -3,43 +3,40 @@ import imagezmq
 import socket
 import cv2
 import time
+import Functions
+from filecheck import FileCheck
 from imutils.video import VideoStream
 
-server_ip = "192.168.1.8"
+server_ip = "192.168.1.24"
 sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(server_ip))
 rpiName = socket.gethostname()
-# vs = VideoStream(src=0).start()
+vs = VideoStream(src=0, resolution=(1280, 720), frame=30.0).start()
 # time.sleep(1.0)
 
 #define size for saving video
 # Get current width of frame
-frame_width = 680
-frame_height = 480
 fps = 15.0
 
-video_capture = cv2.VideoCapture(0)
-video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
-video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
-
-size = (int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)), int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-
+size=(1280, 720)
 fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-result = cv2.VideoWriter('test.mp4', fourcc, fps, size)
+result = cv2.VideoWriter('raspberrypi.mp4', fourcc, fps, size)
 
 while True:
-    ret, frame = video_capture.read()
-    if ret==True:
-        # print('Write Frame')
-        # cv2.imshow("Frame", frame)
+    try:
+        frame = vs.read()
         result.write(frame)
         sender.send_image(rpiName, frame)
-        key = cv2.waitKey (1) & 0xFF
-        if key == ord("q"):
-            break
-    else:
-        break
+    except Exception as err:
+        print("[INFOR]: "+str(err))
 
-# video_capture.release()
+vs.release()
 result.release()
+
+#Delete video if it is larger than 100GB
+filevideo_path='raspberrypi.mp4'
+videocheck=FileCheck(filevideo_path)
+size_video, FLAG=videocheck.get_filesize()
+Functions.delete_video(FLAG, size_video, filevideo_path)
+
 # do a bit of cleanup
 cv2.destroyAllWindows()
